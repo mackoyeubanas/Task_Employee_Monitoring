5a0a27ac54fda5f0abbdc8fc7351b70a3e241a17
mackoyeubanas <90250663+mackoyeubanas@users.noreply.github.com>
2024-01-16 06:18:19 +0000

    UPDATE

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config()
-const mongoose = require("mongoose");
-const app = require('./index');
 
-const DB = process.env.MONGO_URL.replace('<PASSWORD>', process.env.MONGO_PASSWORD);
 
 mongoose.connect(DB)
     .then(() => { console.log('Database working 👍') })
