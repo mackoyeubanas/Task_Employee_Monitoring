88eda6c4abdb469c8244036a43a097f3972ed283
mackoyeubanas <90250663+mackoyeubanas@users.noreply.github.com>
2024-01-16 06:18:28 +0000

    UPDATE

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const dotenv = require('dotenv');
 dotenv.config()
+const mongoose = require("mongoose");
+const app = require('./index');
 
+const connectDB = async()=>{
+    const DB = process.env.MONGO_URL
 
 mongoose.connect(DB)
     .then(() => { console.log('Database working 👍') })
@@ -10,4 +14,7 @@ const port = process.env.PORT || 4000;
 app.listen(port, err => {
     if (err) console.log(err)
     else console.log('CRUD application running on port ' + port + ' 💯');
-});  
+});  }
+
+
+module.exports = {connectDB};
